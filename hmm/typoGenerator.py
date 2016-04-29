d253ce99cbd972c3c07be7d7281c3280f0c2cf89
jiaweiso <jiaweisong92@hotmail.com>
2016-04-28 17:40:01 -0700

    add typo generator and pinyin training

---
+++ hmm/typoGenerator.py
@@ -0,0 +1,162 @@
+import random
+
+def getNew(ori):
+	direc = random.randrange(0,4)
+	#replace by letter on the left
+	if direc == 0:
+		return {
+			'q':'w',
+			'w':'q',
+			'e':'w',
+			'r':'e',
+			't':'r',
+			'y':'t',
+			'u':'y',
+			'i':'u',
+			'o':'i',
+			'p':'o',
+			'a':'s',
+			's':'a',
+			'd':'s',
+			'f':'d',
+			'g':'f',
+			'h':'g',
+			'j':'h',
+			'k':'j',
+			'l':'k',
+			'z':'x',
+			'x':'z',
+			'c':'x',
+			'v':'c',
+			'b':'v',
+			'n':'b',
+			'm':'n',
+		}.get(ori,'')
+
+	#replace by letter on the right
+	elif direc == 1:
+		return {
+			'q':'w',
+			'w':'e',
+			'e':'r',
+			'r':'t',
+			't':'y',
+			'y':'u',
+			'u':'i',
+			'i':'o',
+			'o':'p',
+			'p':'o',
+			'a':'s',
+			's':'d',
+			'd':'f',
+			'f':'g',
+			'g':'h',
+			'h':'j',
+			'j':'k',
+			'k':'l',
+			'l':'k',
+			'z':'x',
+			'x':'c',
+			'c':'v',
+			'v':'b',
+			'b':'n',
+			'n':'m',
+			'm':'n',
+		}.get(ori,'')
+
+	#replace by letter on the top
+	elif direc == 2:
+		return {
+			'q':'a',
+			'w':'s',
+			'e':'d',
+			'r':'f',
+			't':'g',
+			'y':'h',
+			'u':'j',
+			'i':'k',
+			'o':'l',
+			'p':'l',
+			'a':'q',
+			's':'w',
+			'd':'e',
+			'f':'r',
+			'g':'t',
+			'h':'y',
+			'j':'u',
+			'k':'i',
+			'l':'o',
+			'z':'a',
+			'x':'s',
+			'c':'d',
+			'v':'f',
+			'b':'g',
+			'n':'h',
+			'm':'j',
+		}.get(ori,'')
+	#replace by letter on the bottom
+	else:
+		return {
+			'q':'a',
+			'w':'s',
+			'e':'d',
+			'r':'f',
+			't':'g',
+			'y':'h',
+			'u':'j',
+			'i':'k',
+			'o':'l',
+			'p':'l',
+			'a':'z',
+			's':'x',
+			'd':'c',
+			'f':'v',
+			'g':'b',
+			'h':'n',
+			'j':'m',
+			'k':'m',
+			'l':'m',
+			'z':'a',
+			'x':'s',
+			'c':'d',
+			'v':'f',
+			'b':'g',
+			'n':'h',
+			'm':'j',
+		}.get(ori,'')
+
+
+outf = open('typo.txt','w')
+
+with open('testData.txt','r') as f:
+	for s in f:
+		words = s.split()
+		for word in words:
+			ctype = 3#random.randrange(0,4)
+			pos = random.randrange(0,len(word))
+			#add 
+			if ctype == 0:
+				letter = getNew(word[pos])
+				shift = random.randrange(0,2)
+				pos += shift
+				word = word[:pos] + letter + word[pos:]
+			#remove	a letter at random position
+			elif ctype == 1:
+				word = word[:pos] + word[pos+1:]
+			#replace a letter with its neighbor 	
+			elif ctype == 2:
+				newLetter = getNew(word[pos])
+				word = word[:pos] + str(newLetter) + word[pos+1:]
+			#reorder
+			else:
+				letter = word[pos]
+				word = word[:pos] + word[pos+1:]
+				pos = random.randrange(0,len(word)+1)
+				word = word[:pos] + letter + word[pos:]
+			outf.write(word)
+			outf.write(' ')
+		outf.write('\n')
+
+outf.close()
+
+				
---
+++ pinyinTrain/pinyinLearn.py
@@ -0,0 +1,83 @@
+#coding: utf8
+def remove(current):
+	current = current.replace("ā","a")
+	current = current.replace("á","a")
+	current = current.replace("ǎ","a")
+	current = current.replace("à","a")
+	
+	current = current.replace("ī","i")
+	current = current.replace("í","i")
+	current = current.replace("ǐ","i")
+	current = current.replace("ì","i")
+
+	current = current.replace("ē","e")
+	current = current.replace("é","e")
+	current = current.replace("ě","e")
+	current = current.replace("è","e")
+
+	current = current.replace("ō","o")
+	current = current.replace("ó","o")
+	current = current.replace("ǒ","o")
+	current = current.replace("ò","o")	
+
+	current = current.replace("ū","u")
+	current = current.replace("ú","u")
+	current = current.replace("ǔ","u")
+	current = current.replace("ù","u")
+
+	current = current.replace("ǖ","v")
+	current = current.replace("ǘ","v")
+	current = current.replace("ǚ","v")
+	current = current.replace("ǜ","v")
+	return current
+
+def updateTranMap(preW,current,pinyinTran):
+	if preW not in pinyinTran:
+		pinyinTran[preW] = {}
+	if current not in pinyinTran[preW]:
+		pinyinTran[preW][current] = 1
+	else:
+		pinyinTran[preW][current] += 1
+
+
+outf = open('pinyinModel.txt','w')
+
+pinyinTran = {}
+with open('pinyinTrainData.txt','r') as f:
+	for line in f:
+		words = line.strip().split()
+		preW = ""
+		for w in words:
+			w = w.split('(')
+			if ("。" in w[0]) or ("，" in w[0]) or ("！" in w[0]) or ("：" in w[0]) or ("？" in w[0]) or ("......" in w[0]) or ("、" in w[0]) or ('"' in w[0]): 
+				preW = ""
+			if len(w) > 1:
+				current = remove(w[1])
+				updateTranMap(preW,current,pinyinTran)
+				preW = current
+
+
+with open('pinyinTrainData2.txt','r') as f:
+	for line in f:
+		words = line.strip().split()
+		preW = ""
+		for w in words:
+			if ("。" in w) or ("，" in w) or ("！" in w) or ("：" in w) or ("？" in w) or ("......" in w) or ("、" in w) or ('"' in w) or ('“' in w) or ('”' in w): 
+				preW = ""
+			else:
+				current = remove(w)
+				print current
+				updateTranMap(preW,current,pinyinTran)
+				preW = current
+
+
+for word in pinyinTran:
+	outf.write(word)
+	outf.write('\n')
+	for word2 in pinyinTran[word]:
+		outf.write('%12s %12s' % (word2,str(pinyinTran[word][word2])))
+		outf.write('\n')
+
+outf.close()
+
+
